249d67bf1a8a12a403349f827a9fcec7af57bc25
lukas <lukasjonsson@outlook.de>
2023-11-14 21:35:31 +0100

    Changing rootProject

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -9,6 +9,6 @@ plugins {
     id("org.gradle.toolchains.foojay-resolver-convention") version "0.7.0"
 }
 
-rootProject.name = "mccreativelab"
+rootProject.name = "MCCreativeLab"
 
 include("mccreativelab-api", "mccreativelab-server")
