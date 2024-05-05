08112ba1291bbdf1826e6c0bb6c7e95f66eed96d
verdox <mail.ysp@web.de>
2024-05-05 16:38:08 +0000

    Fixed PartlyVisibleTexture not working correctly

--- plugin-extension/src/main/java/de/verdox/mccreativelab/generator/resourcepack/types/rendered/element/group/PartlyVisibleTexture.java
+++ plugin-extension/src/main/java/de/verdox/mccreativelab/generator/resourcepack/types/rendered/element/group/PartlyVisibleTexture.java
@@ -5,6 +5,7 @@ import de.verdox.mccreativelab.generator.resourcepack.types.rendered.ActiveCompo
 import de.verdox.mccreativelab.generator.resourcepack.types.rendered.element.HudElementGroup;
 import de.verdox.mccreativelab.generator.resourcepack.types.rendered.element.single.SingleHudTexture;
 import net.kyori.adventure.text.Component;
+import org.bukkit.Bukkit;
 
 import java.util.List;
 import java.util.stream.Collectors;
@@ -18,32 +19,41 @@ public record PartlyVisibleTexture(List<SingleHudTexture> hudTextures) implement
     @Override
     public RenderedGroup<SingleHudTexture, ? extends HudElementGroup<SingleHudTexture>, ?> toRenderedElement() {
         return new RenderedPartlyVisibleTexture(this, hudTextures.stream().map(SingleHudTexture::toRenderedElement)
-                                                           .map(renderedSingle -> ((SingleHudTexture.RenderedSingleHudTexture) renderedSingle))
-                                                           .collect(Collectors.toList()));
+            .map(renderedSingle -> ((SingleHudTexture.RenderedSingleHudTexture) renderedSingle))
+            .collect(Collectors.toList()));
     }
 
     public static class RenderedPartlyVisibleTexture extends RenderedGroup<SingleHudTexture, PartlyVisibleTexture, SingleHudTexture.RenderedSingleHudTexture> {
+        private int currentShown = -1;
+        private SingleHudTexture.RenderedSingleHudTexture currentShownTexture = null;
+
         public RenderedPartlyVisibleTexture(PartlyVisibleTexture hudElementGroup, List<SingleHudTexture.RenderedSingleHudTexture> groupedRenderedElements) {
             super(hudElementGroup, groupedRenderedElements);
         }
 
         public void show(int partToShow) {
-            if (partToShow < 0 || partToShow > getGroupedRenderedElements().size())
+            if (partToShow < 0 || partToShow > getGroupedRenderedElements().size()) {
+                Bukkit.getLogger().warning("Trying to show the texture part " + partToShow + " that does not exist! Available range from " + 0 + " to " + (getGroupedRenderedElements().size() - 1));
                 return;
-
-            for (int i = 0; i < getGroupedRenderedElements().size(); i++) {
-                var part = getGroupedRenderedElements().get(i);
-
-
-                part.setVisible(i == partToShow - 1);
-                if(part.getHudElement().height() == 1 && part.getHudElement().width() == 1)
-                    part.setVisible(false);
             }
+
+            this.setVisible(true);
+
+            var part = getGroupedRenderedElements().get(partToShow);
+
+            if (currentShown != -1 && currentShown != partToShow)
+                getGroupedRenderedElements().get(currentShown).setVisible(false);
+
+            part.setVisible(true);
+            currentShown = partToShow;
+            currentShownTexture = part;
         }
 
         @Override
-        protected Component doRendering(ActiveComponentRendered<?,?> activeComponentRendered) {
-            return Component.empty();
+        protected Component doRendering(ActiveComponentRendered<?, ?> activeComponentRendered) {
+            if (currentShownTexture == null)
+                return Component.empty();
+            return currentShownTexture.render(activeComponentRendered);
         }
     }
 }
