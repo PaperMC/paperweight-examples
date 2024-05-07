6e1a253463839ef950fe806a32dcc21134f2d08d
verdox <mail.ysp@web.de>
2024-05-07 17:19:46 +0000

    Adding param to DataPackUtil installDataPack

--- plugin-extension/src/main/java/de/verdox/mccreativelab/util/DataPackUtil.java
+++ plugin-extension/src/main/java/de/verdox/mccreativelab/util/DataPackUtil.java
@@ -7,7 +7,7 @@ import java.io.File;
 import java.nio.file.Path;
 
 public class DataPackUtil {
-    public static File installDataPack(String levelName, Asset<?> dataPackAsset){
-        return ZipUtil.extractFilesFromZipFileResource(dataPackAsset.assetInputStream(), String.valueOf(Path.of(levelName+"/datapacks/")));
+    public static File installDataPack(String levelName, String dataPackName, Asset<?> dataPackAsset) {
+        return ZipUtil.extractFilesFromZipFileResource(dataPackAsset.assetInputStream(), String.valueOf(Path.of(levelName + "/datapacks/" + dataPackName)));
     }
 }
