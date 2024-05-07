1c1f7826c373f4f5347750b4d391f3abe308d89d
verdox <mail.ysp@web.de>
2024-05-07 17:18:55 +0000

    Changing command structure

--- plugin-extension/src/main/java/de/verdox/mccreativelab/debug/FakeBlockCommand.java
+++ plugin-extension/src/main/java/de/verdox/mccreativelab/debug/FakeBlockCommand.java
@@ -5,13 +5,17 @@ import de.verdox.mccreativelab.world.block.FakeBlock;
 import de.verdox.mccreativelab.world.block.FakeBlockStorage;
 import de.verdox.mccreativelab.registry.exception.PaletteValueUnknownException;
 import de.verdox.mccreativelab.world.block.replaced.ReplacedBlocks;
+import de.verdox.mccreativelab.world.item.FakeItem;
 import net.kyori.adventure.text.Component;
+import org.bukkit.NamespacedKey;
 import org.bukkit.block.Block;
 import org.bukkit.command.Command;
 import org.bukkit.command.CommandSender;
 import org.bukkit.entity.Player;
 import org.bukkit.util.RayTraceResult;
 import org.jetbrains.annotations.NotNull;
+
+import java.util.List;
 
 public class FakeBlockCommand extends Command {
 
@@ -20,7 +24,7 @@ public class FakeBlockCommand extends Command {
         setPermission("mccreativelab.command.fakeblock");
     }
 
-    @Override
+/*    @Override
     public boolean execute(@NotNull CommandSender sender, @NotNull String commandLabel, @NotNull String[] args) {
         if(!sender.hasPermission("mccreativelab.command.fakeblock"))
             return false;
@@ -57,5 +61,49 @@ public class FakeBlockCommand extends Command {
             }
         }
         return false;
+    }*/
+
+    @Override
+    public boolean execute(@NotNull CommandSender sender, @NotNull String commandLabel, @NotNull String[] args) {
+        if(!sender.hasPermission("mccreativelab.command.fakeblock"))
+            return false;
+        if(!(sender instanceof Player player))
+            return false;
+
+        RayTraceResult rayTraceResult = player.rayTraceBlocks(5);
+        if(rayTraceResult == null)
+            return false;
+        Block block = rayTraceResult.getHitBlock();
+        if(block == null)
+            return false;
+
+        if(args.length == 0) {
+            player.sendMessage("");
+            return false;
+        }
+        if(args[0].equalsIgnoreCase("get")){
+            if(args.length == 2){
+                String keyAsString = args[1];
+                try{
+                    NamespacedKey namespacedKey = NamespacedKey.fromString(keyAsString);
+                    FakeBlock fakeBlock = MCCreativeLabExtension.getFakeBlockRegistry().get(namespacedKey);
+                    FakeBlockStorage.setFakeBlock(block.getLocation(), fakeBlock, false);
+                }
+                catch (Exception e){
+                    sender.sendMessage("Please provide a valid custom item");
+                    return false;
+                }
+            }
+        }
+        return false;
+    }
+
+    @Override
+    public @NotNull List<String> tabComplete(@NotNull CommandSender sender, @NotNull String alias, @NotNull String[] args) throws IllegalArgumentException {
+        if(args.length <= 1)
+            return List.of("get");
+        if(args.length == 2)
+            return MCCreativeLabExtension.getFakeBlockRegistry().streamKeys().map(NamespacedKey::asString).toList();
+        return List.of();
     }
 }
