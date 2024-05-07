a2c4d89fe9233b1b4e5f11146d6b97edcf7a3025
verdox <mail.ysp@web.de>
2024-05-07 22:36:57 +0000

    Refactoring code base to ServerSoftwareExclusives and ExtensionFeatures

--- plugin-extension/src/main/java/de/verdox/mccreativelab/ServerSoftwareExclusives.java
+++ plugin-extension/src/main/java/de/verdox/mccreativelab/ServerSoftwareExclusives.java
@@ -1,8 +1,12 @@
 package de.verdox.mccreativelab;
 
 import de.verdox.mccreativelab.event.MCCreativeLabReloadEvent;
+import de.verdox.mccreativelab.features.legacy.LegacyFeatures;
 import de.verdox.mccreativelab.world.block.FakeBlockRegistry;
+import de.verdox.mccreativelab.world.block.FakeBlockStorage;
 import de.verdox.mccreativelab.world.block.replaced.ReplacedBlocks;
+import de.verdox.mccreativelab.world.item.FakeItemRegistry;
+import de.verdox.mccreativelab.world.sound.ReplacedSoundGroups;
 import org.bukkit.Bukkit;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.EventPriority;
@@ -10,9 +14,19 @@ import org.bukkit.event.Listener;
 import org.bukkit.event.server.ServerLoadEvent;
 
 public class ServerSoftwareExclusives implements Listener {
+    private static FakeBlockRegistry FAKE_BLOCK_REGISTRY;
+    private static FakeItemRegistry FAKE_ITEM_REGISTRY;
+    private static FakeBlockStorage fakeBlockStorage;
+    private static LegacyFeatures legacyFeatures;
+
+    private ReplacedSoundGroups replacedSoundGroups;
 
     public void onLoad(){
-
+        FAKE_BLOCK_REGISTRY = new FakeBlockRegistry();
+        FAKE_ITEM_REGISTRY = new FakeItemRegistry();
+        replacedSoundGroups  = new ReplacedSoundGroups();
+        fakeBlockStorage = new FakeBlockStorage();
+        legacyFeatures = new LegacyFeatures();
     }
 
     public void onEnable(){
@@ -25,10 +39,33 @@ public class ServerSoftwareExclusives implements Listener {
 
     public void onServerLoad(ServerLoadEvent.LoadType loadType){
         if(loadType.equals(ServerLoadEvent.LoadType.STARTUP)){
+            if(MCCreativeLabExtension.getFakeBlockRegistry().hasReusedAnyBlockStates()){
+                if(FakeBlockRegistry.USE_ALTERNATE_FAKE_BLOCK_ENGINE) {
+                    replacedSoundGroups.replaceGlassSoundGroup();
+                }
+                replacedSoundGroups.replaceWoodSoundGroup();
+            }
+
             FakeBlockRegistry.setupFakeBlocks();
             ReplacedBlocks.setup();
             Bukkit.getLogger().info("ServerSoftware exclusive features started");
         }
+    }
+
+    public static FakeBlockRegistry getFakeBlockRegistry() {
+        return FAKE_BLOCK_REGISTRY;
+    }
+
+    public static FakeItemRegistry getFakeItemRegistry() {
+        return FAKE_ITEM_REGISTRY;
+    }
+
+    public static FakeBlockStorage getFakeBlockStorage() {
+        return fakeBlockStorage;
+    }
+
+    public static LegacyFeatures getLegacyFeatures() {
+        return legacyFeatures;
     }
 
     @EventHandler(priority = EventPriority.LOWEST)
@@ -39,5 +76,9 @@ public class ServerSoftwareExclusives implements Listener {
     @EventHandler(priority = EventPriority.LOWEST)
     public void onPluginReload(MCCreativeLabReloadEvent ignored) {
         onServerLoad(ServerLoadEvent.LoadType.RELOAD);
+    }
+
+    public ReplacedSoundGroups getReplacedSoundGroups() {
+        return replacedSoundGroups;
     }
 }
