7acb13769124f95114ced968ea217a8ba2ad32d4
verdox <mail.ysp@web.de>
2024-05-28 23:18:40 +0000

    Updating project to Java 21

--- plugin-extension/build.gradle.kts
+++ plugin-extension/build.gradle.kts
@@ -44,7 +44,7 @@ allprojects {
 
             // Set the release flag. This configures what version bytecode the compiler will emit, as well as what JDK APIs are usable.
             // See https://openjdk.java.net/jeps/247 for more information.
-            options.release.set(17)
+            options.release.set(21)
         }
 
         processResources {
