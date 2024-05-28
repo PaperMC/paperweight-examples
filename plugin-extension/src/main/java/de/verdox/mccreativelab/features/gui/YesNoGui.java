e0ac2320ba745556261f712aba251c27bed7076d
verdox <mail.ysp@web.de>
2024-05-28 16:48:39 +0000

    Changes to YesNoGui

--- plugin-extension/src/main/java/de/verdox/mccreativelab/features/gui/YesNoGui.java
+++ plugin-extension/src/main/java/de/verdox/mccreativelab/features/gui/YesNoGui.java
@@ -3,7 +3,7 @@ package de.verdox.mccreativelab.features.gui;
 import de.verdox.mccreativelab.generator.resourcepack.types.gui.ClickableItem;
 import de.verdox.mccreativelab.generator.resourcepack.types.gui.CustomGUIBuilder;
 import de.verdox.mccreativelab.generator.resourcepack.types.lang.LanguageInfo;
-import de.verdox.mccreativelab.generator.resourcepack.types.lang.Translatable;
+import de.verdox.mccreativelab.generator.resourcepack.types.lang.Translation;
 import de.verdox.mccreativelab.generator.resourcepack.types.rendered.RenderedElementBehavior;
 import de.verdox.mccreativelab.generator.resourcepack.types.rendered.util.ScreenPosition;
 import de.verdox.mccreativelab.util.io.StringAlign;
@@ -17,15 +17,15 @@ import java.util.function.Consumer;
 public class YesNoGui extends CustomGUIBuilder {
     private static YesNoGui INSTANCE;
 
-    public static final Translatable YES = new Translatable(LanguageInfo.ENGLISH_US, "yes", "Yes");
-    public static final Translatable NO = new Translatable(LanguageInfo.ENGLISH_US, "no", "No");
+    public static final Translation YES = new Translation(LanguageInfo.ENGLISH_US, "yes", "Yes");
+    public static final Translation NO = new Translation(LanguageInfo.ENGLISH_US, "no", "No");
 
     public YesNoGui() {
         super(new NamespacedKey("oneblock", "yes_no_gui"), 5);
         INSTANCE = this;
 
         withMultiLineText("question", 10, 30, 8, StringAlign.Alignment.CENTER, new ScreenPosition(50, 50, 0, 80, 1), 1f,
-            RenderedElementBehavior.createWhileOpen((activeGUI, player, renderedGroupMultiLineText) -> {
+            RenderedElementBehavior.createWhileOpen((activeGUI, renderedGroupMultiLineText) -> {
 
                 String question = activeGUI.getTemporaryDataOrDefault("question", String.class, "This is an example question to show how the YesNoGui looks like!");
                 if(question == null || question.isEmpty()) {
@@ -39,8 +39,8 @@ public class YesNoGui extends CustomGUIBuilder {
 
     public static class Builder {
         private String question;
-        private ClickableItem.Builder yesButton = new ClickableItem.Builder(Material.LIME_DYE).withItemMeta(itemMeta -> itemMeta.displayName(Component.translatable(YES.key())));
-        private ClickableItem.Builder noButton = new ClickableItem.Builder(Material.RED_DYE).withItemMeta(itemMeta -> itemMeta.displayName(Component.translatable(NO.key())));
+        private final ClickableItem.Builder yesButton = new ClickableItem.Builder(Material.LIME_DYE).withItemMeta(itemMeta -> itemMeta.displayName(Component.translatable(YES.key()))).closeGUI();
+        private final ClickableItem.Builder noButton = new ClickableItem.Builder(Material.RED_DYE).withItemMeta(itemMeta -> itemMeta.displayName(Component.translatable(NO.key()))).closeGUI();
 
         public Builder withQuestion(String question) {
             this.question = question;
