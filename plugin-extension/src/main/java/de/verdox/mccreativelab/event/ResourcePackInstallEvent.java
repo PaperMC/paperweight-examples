72a49832077562cdf29e7d90385ba57699394c0d
verdox <mail.ysp@web.de>
2024-08-28 22:52:53 +0000

    Removing old code

---
+++ plugin-extension/src/main/java/de/verdox/mccreativelab/event/ResourcePackInstallEvent.java
@@ -0,0 +1,18 @@
+package de.verdox.mccreativelab.event;
+
+import org.bukkit.event.Event;
+import org.bukkit.event.HandlerList;
+import org.jetbrains.annotations.NotNull;
+
+public class ResourcePackInstallEvent extends Event {
+    private static final HandlerList HANDLERS = new HandlerList();
+    @NotNull
+    public static HandlerList getHandlerList() {
+        return HANDLERS;
+    }
+
+    @NotNull
+    public HandlerList getHandlers() {
+        return HANDLERS;
+    }
+}
